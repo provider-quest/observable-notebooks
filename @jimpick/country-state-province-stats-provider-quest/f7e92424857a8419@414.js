6f5d5b6d353dd8b5856a72c5216a0c5f9f33a8f3
backup-to-github <robot@webcode.run>
2022-03-30 06:00:05 +0000

    Backup https://observablehq.com/@jimpick/country-state-province-stats-provider-quest   url:     https://observablehq.com/@jimpick/country-state-province-stats-provider-quest   title:   Country/State/Province Stats [Provider.Quest]   author:  Jim Pick (@jimpick)   id:      f7e92424857a8419   version: 414

--- @jimpick/country-state-province-stats-provider-quest/f7e92424857a8419@414.js
+++ @jimpick/country-state-province-stats-provider-quest/f7e92424857a8419@414.js
@@ -1,5 +1,5 @@
 // https://observablehq.com/@jimpick/country-state-province-stats-provider-quest@414
-import define1 from "./5cf93b57a7444002@185.js";
+import define1 from "./5cf93b57a7444002@196.js";
 import define2 from "./c4e4a355c53d2a1a@111.js";
 
 function _1(md){return(
