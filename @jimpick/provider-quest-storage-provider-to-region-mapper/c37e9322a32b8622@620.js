b9178c426ed685dc16add7035042256529d0d5f6
backup-to-github <robot@webcode.run>
2022-04-14 01:44:00 +0000

    Backup https://observablehq.com/@jimpick/provider-quest-storage-provider-to-region-mapper   url:     https://observablehq.com/@jimpick/provider-quest-storage-provider-to-region-mapper   title:   Internal: Mapping: Storage Provider to Region [Provider.Quest]   author:  Jim Pick (@jimpick)   id:      c37e9322a32b8622   version: 620

--- @jimpick/provider-quest-storage-provider-to-region-mapper/c37e9322a32b8622@620.js
+++ @jimpick/provider-quest-storage-provider-to-region-mapper/c37e9322a32b8622@620.js
@@ -1,9 +1,9 @@
 // https://observablehq.com/@jimpick/provider-quest-storage-provider-to-region-mapper@620
-import define1 from "./5cf93b57a7444002@196.js";
-import define2 from "./5cf93b57a7444002@196.js";
+import define1 from "./5cf93b57a7444002@222.js";
+import define2 from "./5cf93b57a7444002@222.js";
 import define3 from "./a957eb792b00ff81@406.js";
 import define4 from "./c4e4a355c53d2a1a@111.js";
-import define5 from "./13063df7b34879ca@853.js";
+import define5 from "./13063df7b34879ca@856.js";
 
 function _1(md){return(
 md`# Internal: Mapping: Storage Provider to Region [Provider.Quest]`
