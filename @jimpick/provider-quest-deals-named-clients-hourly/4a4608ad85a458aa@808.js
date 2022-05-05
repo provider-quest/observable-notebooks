15c1247ead3d199c8253a7b0e25fe3af5c54a0b4
backup-to-github <robot@webcode.run>
2022-05-05 07:05:19 +0000

    Backup https://observablehq.com/@jimpick/provider-quest-deals-named-clients-hourly   url:     https://observablehq.com/@jimpick/provider-quest-deals-named-clients-hourly   title:   Published Deals: Named Clients: Hourly [Provider.Quest]   author:  Jim Pick (@jimpick)   id:      4a4608ad85a458aa   version: 808

--- @jimpick/provider-quest-deals-named-clients-hourly/4a4608ad85a458aa@808.js
+++ @jimpick/provider-quest-deals-named-clients-hourly/4a4608ad85a458aa@808.js
@@ -1,5 +1,5 @@
 // https://observablehq.com/@jimpick/provider-quest-deals-named-clients-hourly@808
-import define1 from "./5cf93b57a7444002@196.js";
+import define1 from "./5cf93b57a7444002@222.js";
 import define2 from "./f92778131fd76559@1173.js";
 import define3 from "./c4e4a355c53d2a1a@111.js";
 
