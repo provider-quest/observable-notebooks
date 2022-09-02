71fdb3c3da7164e0cc7e0b0c91b882a9e3ac8671
backup-to-github <robot@webcode.run>
2022-09-02 19:34:48 +0000

    Backup https://observablehq.com/@jimpick/provider-quest-miner-power-scanner   url:     https://observablehq.com/@jimpick/provider-quest-miner-power-scanner   title:   Internal: Scanner: Power [Provider.Quest]   author:  Jim Pick (@jimpick)   id:      6acaea5ce7388dfd   version: 1929

--- @jimpick/provider-quest-miner-power-scanner/6acaea5ce7388dfd@1929.js
+++ @jimpick/provider-quest-miner-power-scanner/6acaea5ce7388dfd@1929.js
@@ -1,5 +1,5 @@
 // https://observablehq.com/@jimpick/provider-quest-miner-power-scanner@1929
-import define1 from "./5cf93b57a7444002@230.js";
+import define1 from "./5cf93b57a7444002@282.js";
 import define2 from "./a957eb792b00ff81@406.js";
 import define3 from "./c4e4a355c53d2a1a@111.js";
 
