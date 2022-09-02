cc54fabea7911d1be3c506710a2413efe7014769
backup-to-github <robot@webcode.run>
2022-09-02 19:34:43 +0000

    Backup https://observablehq.com/@jimpick/provider-quest-miners-on-a-global-map   url:     https://observablehq.com/@jimpick/provider-quest-miners-on-a-global-map   title:   Global Map [Provider.Quest]   author:  Jim Pick (@jimpick)   id:      3bbd19478bdde1c5   version: 773

--- @jimpick/provider-quest-miners-on-a-global-map/3bbd19478bdde1c5@773.js
+++ @jimpick/provider-quest-miners-on-a-global-map/3bbd19478bdde1c5@773.js
@@ -1,5 +1,5 @@
 // https://observablehq.com/@jimpick/provider-quest-miners-on-a-global-map@773
-import define1 from "./5cf93b57a7444002@246.js";
+import define1 from "./5cf93b57a7444002@282.js";
 import define2 from "./c4e4a355c53d2a1a@111.js";
 
 function _1(md){return(
