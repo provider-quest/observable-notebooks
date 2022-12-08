bd55240eb1d875f0b0bb15ca0e88713a4dc863e1
backup-to-github <robot@webcode.run>
2022-12-08 21:57:38 +0000

    Backup https://observablehq.com/@jimpick/fvm-actor-code-playground-erc20-sans-events   url:     https://observablehq.com/@jimpick/fvm-actor-code-playground-erc20-sans-events   title:   FVM Actor Code Playground - ERC20 Sans Events   author:  Jim Pick (@jimpick)   id:      c7e7e39ebd4ccf64   version: 1912

--- @jimpick/fvm-actor-code-playground-erc20-sans-events/c7e7e39ebd4ccf64@1912.js
+++ @jimpick/fvm-actor-code-playground-erc20-sans-events/c7e7e39ebd4ccf64@1912.js
@@ -802,7 +802,7 @@ function _initialCodeUrl(){return(
 )}
 
 function _baseUrl(){return(
-"https://fvm-11.default.knative.hex.camp"
+"https://fvm-12.default.knative.hex.camp"
 )}
 
 async function _token(baseUrl){return(
