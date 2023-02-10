7fb4c648f49f0827d19bf4e7e5d89704825c499c
backup-to-github <robot@webcode.run>
2023-02-10 10:58:03 +0000

    Backup https://observablehq.com/@jimpick/test-github-backup   url:     https://observablehq.com/@jimpick/test-github-backup   title:   Test GitHub backup   author:  Jim Pick (@jimpick)   id:      0cba1e4a9544bea3   version: 22

---
+++ @jimpick/test-github-backup/0cba1e4a9544bea3@22.js
@@ -0,0 +1,40 @@
+// https://observablehq.com/@jimpick/test-github-backup@22
+import define1 from "./1d309dbd9697e042@631.js";
+
+function _1(md){return(
+md`# Test GitHub backup`
+)}
+
+function _2(md){return(
+md`* https://observablehq.com/@tomlarkworthy/github-backups
+* https://github.com/provider-quest/observable-notebooks`
+)}
+
+function _3(backupNowButton){return(
+backupNowButton()
+)}
+
+function _4(md){return(
+md`Test backup 1`
+)}
+
+function _6(enableGithubBackups){return(
+enableGithubBackups({
+  owner: "provider-quest",                   // Target Github username/organization
+  repo: "observable-notebooks",                // Target Github repo
+  allow: ['jimpick', 'endpointservices'] // [optional] Allowed source observablehq logins
+})
+)}
+
+export default function define(runtime, observer) {
+  const main = runtime.module();
+  main.variable(observer()).define(["md"], _1);
+  main.variable(observer()).define(["md"], _2);
+  main.variable(observer()).define(["backupNowButton"], _3);
+  main.variable(observer()).define(["md"], _4);
+  const child1 = runtime.module(define1);
+  main.import("enableGithubBackups", child1);
+  main.import("backupNowButton", child1);
+  main.variable(observer()).define(["enableGithubBackups"], _6);
+  return main;
+}
---
+++ @jimpick/test-github-backup/index.js
@@ -0,0 +1,1 @@
+export {default} from "./0cba1e4a9544bea3@22.js";
