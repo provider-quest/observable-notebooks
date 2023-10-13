160366cea0502a8031f1d160b56beb202dc11167
backup-to-github <robot@webcode.run>
2023-10-13 22:17:51 +0000

    Backup https://observablehq.com/d/8a7edbcf88cb0f13   url:     https://observablehq.com/d/8a7edbcf88cb0f13   title:   Internal: Scanner: Deals [Provider.Quest]   author:  Jim Pick (@jimpick)   id:      8a7edbcf88cb0f13   version: 1478

---
+++ d/8a7edbcf88cb0f13/15fa16fc510b5dec@200.js
@@ -0,0 +1,94 @@
+// https://observablehq.com/@jimpick/filecoin-decode-deal-params@200
+function _1(md){return(
+md`# Filecoin Decode Deal Params`
+)}
+
+function _messageCid(){return(
+'bafy2bzacebv6cxckmf66rks2xfbezm2gmklgqv2luw4ujx2ttyyotx65ibbhi'
+)}
+
+function _3(md,messageCid){return(
+md`[Message on Filfox](https://filfox.info/en/message/${messageCid})`
+)}
+
+function _params(){return(
+"gYaCi9gqWCgAAYHiA5IgIPtXEp9ExL0uMkiAlxia8Yi+106Q5Nl9OfX2cFe4OXgiGwAAAAgAAAAA9FgxA6YHgwTv6oL6C7mi/KEDTeD3Do4tFW9vM5YJuAVKwmNIZbQ7wbweNdE5kmCAFPpCdUQAqbsBeDRtQVhDZzVBSWdXVjlPaFNNRlg2NkNkakhZZWxaQ25aQ3BRSFdsM013VlhQcmVzUVNzMzAwGgANnpgaACMRDEUAHv6SAEgAHMP16KD470BYYQKxlJcbFiyoosM+mr7rimOjNJ/cHxVI2kg1qu0hLz9O+DgZcZJ0NU6EtjNoCnE8KuEZEdVRPUw2kt9i59pIk70hk24WGbPRdpflKPQkrCZIilWpag0QqgWU6iuOLaTOIkSCi9gqWCgAAYHiA5IgIDBIN438KK84l//xNwSC/qgDx15BRmIv6zfj9RSu7hcDGwAAAAgAAAAA9FgxA6YHgwTv6oL6C7mi/KEDTeD3Do4tFW9vM5YJuAVKwmNIZbQ7wbweNdE5kmCAFPpCdUQAqbsBeDRtQVhDZzVBSWdheVRXTEtObkRzUk9yZUJObFpqRFRPa203Yy9USmZYbkUza3puOUtNTHZvGgANnpgaACMRDEUAHv6SAEgAHMP3G/Zud0BYYQKm2cUtTIkvo5njvtoTfLArjnj+pjgatQskXMgv31rHVoPqO2R3AxdA4PsSFfmUS5EEbdYY4puvXXAHEl5AzMpubnpCbvSkM6jHvp642x9v/rI7IzMGvummEgSFt2DZIEuCi9gqWCgAAYHiA5IgIIZyRpjY3QFmPvfjUceYfIi6d6TkIXoidTr9w4LRe5sDGwAAAAgAAAAA9FgxA6YHgwTv6oL6C7mi/KEDTeD3Do4tFW9vM5YJuAVKwmNIZbQ7wbweNdE5kmCAFPpCdUQAqbsBeDRtQVhDZzVBSWczN2wrcHNtUnVHNGRxbjRYTk5kQjhrL2ZkQU9IOEdJeEJHa2ovUDc5bkdzGgANnpwaACMRDEUAHv6SAEgAHMO46PEYeEBYYQKz4d6JyHiDFj8BqfaJ5HfcgVO7P7TlZjFfGDyRIH1jYKnZ34tLYZyxqKuCchpvSTUBRD2+mjH8cOLVgd0HgnJ4DBa5ZKv6YNItzvbYEkC+3pyVyh4Y6uv4MenFd70AQSyCi9gqWCgAAYHiA5IgIO0cAmwWOckRtDpN5NudU0ohoEfiwImGLCQSGPjWxSkgGwAAAAgAAAAA9FgxA6YHgwTv6oL6C7mi/KEDTeD3Do4tFW9vM5YJuAVKwmNIZbQ7wbweNdE5kmCAFPpCdUQAqbsBeDRtQVhDZzVBSWdSK1p6eWQxeEJ1RnV6MElwUjMxODIremJMcVdwL0dBL3VtVExIa2FHWXZ3GgANnpgaACMRDEUAHv6SAEgAHMP16KD470BYYQKj8ZtTvOI5iucL4wLs6GDNtQDrB9gkn+uGwtRIrXgaTPMhOg4S205K2ZUNIsmkpGgByQ4wQqUstpL1OIUNlov6j8+NZ7M4N/6TgGcgxeXPqys1X8CwpjdgWCvHWqY7atKCi9gqWCgAAYHiA5IgIK4mrdtOBGbvnzaqRyGYsVbWKyVnmXKFEQi1SGH60icJGwAAAAgAAAAA9FgxA7Xg9qKkEDV8s62KxpmkiyLDBa6Jo07uAORq3HJgu9b903QTNhivQjUxNUq9M3iO8EQAqbsBeDRtQVhDZzVBSWdWRWpxaXhGZ2NwVWZuM0hiSXpwTFFyNGNxSElWTGljTGZvOGpLeTF0QjB3GgANnpgaACMRDEUAHc1lAEgAHMP3G/Zud0BYYQKgHKcO2+CkG+rrHxwxx0iVrS1KvYcpD4kHXblNG60sXcZwuW/QRP7yjPC6wI9VfDsQmVeOHWpkjWWDBQOzCAnMurO9kv2cNmgNNoSRnkpaLegeyiWjjpqBaFQ2QgBTd7GCi9gqWCgAAYHiA5IgIEhyPfqDh04woh2dMlCNTiNLlpawcJUHSSbV9ArViuQBGwAAAAgAAAAA9FgxA7a+koTsc/Tiwa6rBxiSrUxx2HiWbqPz9XjRnfvdbdM/9lw5vtbwM9X6p3WWvBj4+kQAqbsBeDRtQVhDZzVBSWdpSDZneTFFVUNDTXdBd29zWUdKaVY1bHIyTTNzK05aa3doVGRRMTQ3NHF3GgANnpoaACMRDEUAHc1lAEgAHMPblV8qNUBYYQKrcgXhFGq9TVLJoQadZpzb55uuW4wyjgAywrSxb7f0kVc3Hgggw5QC/vSDyI56xT0ShBj/SNLI4usZ2atRFvc3LFC3vS7BhBo4Xbvath8pV/hOx8WGLGcVvx1kWT69Uww="
+)}
+
+function _cbor(){return(
+import('https://cdn.skypack.dev/borc')
+)}
+
+function _filecoinNumber(){return(
+import('https://cdn.skypack.dev/@glif/filecoin-number')
+)}
+
+function _BN(require){return(
+require('https://bundle.run/bn.js@5.2.0')
+)}
+
+function _bytesToBig(BN){return(
+function bytesToBig (p) { // https://github.com/spacegap/spacegap.github.io/blob/ccfa30a3e5303c4538c59f3a23186882eddf810e/src/services/filecoin/index.js#L145
+  let acc = new BN(0)
+  for (let i = 0; i < p.length; i++) {
+    acc = acc.mul(new BN(256))
+    acc = acc.add(new BN(p[i]))
+  }
+  return acc
+}
+)}
+
+function _filecoinAddress(){return(
+import('https://cdn.skypack.dev/@glif/filecoin-address')
+)}
+
+async function _CID(){return(
+(await import('https://jspm.dev/cids')).default
+)}
+
+function _decoded(cbor,params){return(
+cbor.decode(params, 'base64')
+)}
+
+function _decodeDeals(cbor,CID,filecoinAddress,bytesToBig){return(
+function decodeDeals (params) {
+  const cborData = cbor.decode(params, 'base64')
+  return cborData[0].map(([cborProposal, cborSignature]) => {
+    return {
+      pieceCid: (new CID(cborProposal[0].value.slice(1))).toString(),
+      pieceSize: cborProposal[1],
+      verifiedDeal: cborProposal[2],
+      client: (new filecoinAddress.Address(cborProposal[3])).toString(),
+      provider: (new filecoinAddress.Address(cborProposal[4])).toString(),
+      label: cborProposal[5],
+      startEpoch: cborProposal[6],
+      endEpoch: cborProposal[7],
+      storagePricePerEpoch: bytesToBig(cborProposal[8]).toString(),
+      providerCollateral: bytesToBig(cborProposal[9]).toString(),
+      clientCollateral: bytesToBig(cborProposal[10]).toString()
+    }
+  })
+}
+)}
+
+function _deals(decodeDeals,params){return(
+decodeDeals(params)
+)}
+
+export default function define(runtime, observer) {
+  const main = runtime.module();
+  main.variable(observer()).define(["md"], _1);
+  main.variable(observer("messageCid")).define("messageCid", _messageCid);
+  main.variable(observer()).define(["md","messageCid"], _3);
+  main.variable(observer("params")).define("params", _params);
+  main.variable(observer("cbor")).define("cbor", _cbor);
+  main.variable(observer("filecoinNumber")).define("filecoinNumber", _filecoinNumber);
+  main.variable(observer("BN")).define("BN", ["require"], _BN);
+  main.variable(observer("bytesToBig")).define("bytesToBig", ["BN"], _bytesToBig);
+  main.variable(observer("filecoinAddress")).define("filecoinAddress", _filecoinAddress);
+  main.variable(observer("CID")).define("CID", _CID);
+  main.variable(observer("decoded")).define("decoded", ["cbor","params"], _decoded);
+  main.variable(observer("decodeDeals")).define("decodeDeals", ["cbor","CID","filecoinAddress","bytesToBig"], _decodeDeals);
+  main.variable(observer("deals")).define("deals", ["decodeDeals","params"], _deals);
+  return main;
+}
