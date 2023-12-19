b944b65f1c2b3e54daa26eff5b986823d49a6185
backup-to-github <robot@webcode.run>
2023-12-19 08:23:14 +0000

    Backup https://observablehq.com/@jimpick/provider-quest-publish-deal-messages-stream   url:     https://observablehq.com/@jimpick/provider-quest-publish-deal-messages-stream   title:   Internal: Scanner: Deals [Provider.Quest]   author:  Jim Pick (@jimpick)   id:      e9d56af58af6ac11   version: 1479

--- @jimpick/provider-quest-publish-deal-messages-stream/e9d56af58af6ac11@1479.js
+++ @jimpick/provider-quest-publish-deal-messages-stream/e9d56af58af6ac11@1479.js
@@ -1,6 +1,6 @@
 import define1 from "./15fa16fc510b5dec@200.js";
 import define2 from "./a957eb792b00ff81@406.js";
-import define3 from "./c4e4a355c53d2a1a@111.js";
+import define3 from "./c4e4a355c53d2a1a@112.js";
 
 function _1(md){return(
 md`# Internal: Scanner: Deals [Provider.Quest]`
