7f16f949718486ad49c72815e06e3a61c2a4917e
backup-to-github <robot@webcode.run>
2024-03-15 13:14:09 +0000

    Backup https://observablehq.com/@jimpick/provider-quest-published-deals-daily   url:     https://observablehq.com/@jimpick/provider-quest-published-deals-daily   title:   Published Deals: Daily [Provider.Quest]   author:  Jim Pick (@jimpick)   id:      a558fb3444ef7dac   version: 432

--- @jimpick/provider-quest-published-deals-daily/a558fb3444ef7dac@432.js
+++ @jimpick/provider-quest-published-deals-daily/a558fb3444ef7dac@432.js
@@ -1,5 +1,5 @@
 import define1 from "./5cf93b57a7444002@284.js";
-import define2 from "./c4e4a355c53d2a1a@111.js";
+import define2 from "./c4e4a355c53d2a1a@112.js";
 
 function _1(md){return(
 md`# Published Deals: Daily [Provider.Quest]`
